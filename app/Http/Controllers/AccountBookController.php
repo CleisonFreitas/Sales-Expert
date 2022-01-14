9ec55442ffa7e1603a10b26a51590abf32a877f0
CleisonFreitas <cleison.ferreira@aldesenvolvimento.com.br>
2022-01-13 22:13:11 -0300

    Adição de Validações ao fechar e abrir o caixa

--- app/Http/Controllers/AccountBookController.php
+++ app/Http/Controllers/AccountBookController.php
@@ -53,7 +53,9 @@ class AccountBookController extends Controller
     public function store(OpenBookRequest $request)
     {
         try{
-            $account_book = AccountBook::create($request->all());
+            if($request->data_aber != null || $request->data_aber != ''){
+                $account_book = AccountBook::create($request->all());
+            }
         }catch(\Exception $e){
             return redirect()->back()->with([toast()->error($e->getMessage())]);
         }
@@ -85,21 +87,35 @@ class AccountBookController extends Controller
      */
     public function close(Request $request,$id)
     {
-        $account_book = AccountBook::find($id);
-        $account_book->data_fech = $request->input('data_fech');
-        switch($account_book->data_fech){
-            case(null);
-                return redirect()->back()->with([toast()->error("Data de fechamento é obrigatório!")]);
-            break;
-            case($account_book->data_fech < $account_book->data_aber);
-                return redirect()->back()->with([toast()->error("Data de fechamento não pode ser menor do que a data de abertura!")]);
-            break;
+        try{
+            DB::beginTransaction();
+            // Localizando Caixa
+            $account_book = AccountBook::find($id);
+
+            // Verificando se o campo "data_fech" foi enviado e se é válido
+            $account_book->data_fech = $request->input('data_fech');
+            switch($account_book->data_fech){
+                case(null);
+                DB::rollback();
+                    return redirect()->back()->with([toast()->error("Data de fechamento é obrigatório!")]);
+                break;
+                case($account_book->data_fech < $account_book->data_aber);
+                DB::rollback();
+                    return redirect()->back()->with([toast()->error("Data de fechamento não pode ser menor do que a data de abertura!")]);
+                break;
+            }
+
+            $account_book->update();
+            DB::commit();
+
+        }catch(Exception $e){
+            DB::rollback();
+            return redirect()->back()->with([toast()->error($e->getMessage())]);
         }
-        $account_book->update();
-        if($account_book == true){
-            return redirect()->route("account.book")->with([toast()->info("Caixa fechado com sucesso!")]);
-        }
-        return redirect()->back()->with([toast()->error("Erro ao tentar fechar caixa!")]);
+
+        return redirect()->route("account.book")->with([toast()->info("Caixa fechado com sucesso!")]);
+
+
     }
 
     /**
